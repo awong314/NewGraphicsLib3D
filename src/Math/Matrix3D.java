58f86ae41ef7c738952e0f5241331c206a848c24
awong314 <andrewwong@csus.edu>
2018-11-04 17:14:18 -0800

    Added function definition for creating translation, scaling and partially for projection

--- src/Math/Matrix3D.java
+++ src/Math/Matrix3D.java
@@ -14,13 +14,36 @@ public class Matrix3D {
 	}
 
 	public Matrix3D(float[] matrix) {
-		for (float f: matrix) {
-
-        }
+		this.values = matrix;
 	}
 
-    public Matrix3D(Vector3D x, Vector3D y, Vector3D z) {
-	    /*TODO*/
+    public Matrix3D(Vector3D xVec, Vector3D yVec, Vector3D zVec) {
+        switch (this.major) {
+            case ROW:
+                this.values = new float[] {
+                        xVec.getX(), xVec.getY(), xVec.getZ(), 0,
+                        yVec.getX(), yVec.getY(), yVec.getZ(), 0,
+                        zVec.getX(), zVec.getY(), zVec.getZ(), 0,
+                            0,           0,           0,       0
+                    };
+                break;
+            case COLUMN:
+                this.values = new float[] {
+                        xVec.getX(), yVec.getX(), zVec.getX(), 0,
+                        xVec.getY(), yVec.getY(), zVec.getY(), 0,
+                        xVec.getZ(), yVec.getZ(), zVec.getZ(), 0,
+                            0,           0,           0,       0
+                };
+                break;
+            default:
+                try {
+                    throw new Exception("Major type for Matrix3D class is invalid");
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                break;
+        }
     }
 
     public Matrix3D createRotationMatrix(float degrees, Vector3D axis) {
@@ -29,31 +52,154 @@ public class Matrix3D {
 	}
 
 	public Matrix3D createRotationMatrix(float amtX, float amtY, float amtZ) {
-	    /*TODO*/
-        return null;
+        switch (this.major) {
+            case ROW:
+                this.values = new float[] {
+                        1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1
+                };
+                break;
+            case COLUMN:
+                this.values = new float[] {
+                        1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1
+                };
+                break;
+            default:
+                try {
+                    throw new Exception("Major type for Matrix3D class is invalid");
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                break;
+        }
+        return this;
 	}
 
     public Matrix3D createScalingMatrix(float amtX, float amtY, float amtZ) {
-	    /*TODO*/
-	    return null;
+        switch (this.major) {
+            case ROW:
+            case COLUMN:
+                this.values = new float[] {
+                        amtX,  0,   0,  0,
+                        0, amtY,  0,  0,
+                        0,   0, amtZ, 0,
+                        0,   0,   0,  1
+                };
+                break;
+            default:
+                try {
+                    throw new Exception("Major type for Matrix3D class is invalid");
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                break;
+        }
+        return this;
 	}
 
     public Matrix3D createTranslationMatrix(float amtX, float amtY, float amtZ) {
-        /*TODO*/
-        return null;
+        switch (this.major) {
+            case ROW:
+                this.values = new float[] {
+                        1, 0, 0, amtX,
+                        0, 1, 0, amtY,
+                        0, 0, 1, amtZ,
+                        0, 0, 0,  1
+                };
+                break;
+            case COLUMN:
+                this.values = new float[] {
+                          1,    0,    0,  0,
+                          0,    1,    0,  0,
+                          0,    0,    1,  0,
+                        amtX, amtY, amtZ, 1
+                };
+                break;
+            default:
+                try {
+                    throw new Exception("Major type for Matrix3D class is invalid");
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                break;
+        }
+        return this;
     }
 
     public Matrix3D createProjectionMatrix(float fov, float width, float height, float near, float far) {
-        /*TODO*/
-        return null;
+        float aspect = width/height;
+        float q = 1.0f / ((float) Math.tan(Math.toRadians(0.5f * fov)));
+        float A = q / aspect;
+        float B = (near + far) / (near - far);
+        float C = (2.0f * near * far) / (near - far);
+
+        switch (this.major) {
+            case ROW:
+                this.values = new float[] {
+                        1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1
+                };
+                break;
+            case COLUMN:
+                this.values = new float[] {
+                        1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1
+                };
+                break;
+            default:
+                try {
+                    throw new Exception("Major type for Matrix3D class is invalid");
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                break;
+        }
+        return this;
     }
 
     public Matrix3D createCameraMatrix(Vector3D forward, Vector3D up) {
-        /*TODO*/
-        return null;
+        switch (this.major) {
+            case ROW:
+                this.values = new float[] {
+                        1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1
+                };
+                break;
+            case COLUMN:
+                this.values = new float[] {
+                        1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1
+                };
+                break;
+            default:
+                try {
+                    throw new Exception("Major type for Matrix3D class is invalid");
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                break;
+        }
+        return this;
     }
 
-	//TODO Fix math
 	public Matrix3D add(Matrix3D mat2) {
 		float[] mat1Floats = this.getValues();
         float[] mat2Floats = mat2.getValues();
@@ -65,12 +211,36 @@ public class Matrix3D {
 			}
 		}
 		else {
-			System.out.println("Length mismatch cannot add matrices of different dimensions");
-			System.exit(0);
-		}
-		
+            try {
+                throw new Exception("Length mismatch cannot add matrices of different dimensions");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
 		return new Matrix3D(temp);
 	}
+
+    public Matrix3D sub(Matrix3D mat2) {
+        float[] mat1Floats = this.getValues();
+        float[] mat2Floats = mat2.getValues();
+        float[] temp = new float[mat1Floats.length];
+
+        if (mat1Floats.length == mat2Floats.length) {
+            for (int i=0; i<mat1Floats.length; i++) {
+                temp[i] = mat1Floats[i] - mat2Floats[i];
+            }
+        }
+        else {
+            try {
+                throw new Exception("Length mismatch cannot subtract matrices of different dimensions");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        return new Matrix3D(temp);
+    }
 
 	public float elementAt(int row, int col) {
 	    /*TODO Derek*/
@@ -151,10 +321,6 @@ public class Matrix3D {
         /*TODO Mutate current*/
         return null;
     }
-
-	public Matrix3D sub(Matrix3D mat2) {
-		return null;
-	}
 	
 	public Matrix3D mult(Matrix3D mat2) {
 		/*TODO Derek*/
@@ -184,9 +350,9 @@ public class Matrix3D {
 	
 	public String toString() {
 		return "This is a 4x4 matrix with the values: \n" +
-				this.values[0]  + " " + this.values[1]  + " " + this.values[2]  + this.values[3]  + "\n" +
-				this.values[4]  + " " + this.values[5]  + " " + this.values[6]  + this.values[7]  + "\n" +
-				this.values[8]  + " " + this.values[9]  + " " + this.values[10] + this.values[11] + "\n" +
-				this.values[12] + " " + this.values[13] + " " + this.values[14] + this.values[15] + "\n";
+				this.values[0]  + " " + this.values[1]  + " " + this.values[2]  + " " + this.values[3]  + "\n" +
+				this.values[4]  + " " + this.values[5]  + " " + this.values[6]  + " " + this.values[7]  + "\n" +
+				this.values[8]  + " " + this.values[9]  + " " + this.values[10] + " " + this.values[11] + "\n" +
+				this.values[12] + " " + this.values[13] + " " + this.values[14] + " " + this.values[15] + "\n";
 	}
 }
--- src/Math/Test.java
+++ src/Math/Test.java
@@ -5,5 +5,11 @@ public class Test {
         Vector3D vec1 = new Vector3D(1,1,1);
         Vector3D vec2 = new Vector3D(1,1,1);
         System.out.println(vec1.add(vec2));
+
+        Matrix3D mat1 = new Matrix3D();
+        Matrix3D mat2 = new Matrix3D();
+
+        System.out.println(mat1.add(mat2));
+        System.out.println(mat1.sub(mat2));
     }
 }

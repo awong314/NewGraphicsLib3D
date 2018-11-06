e434a2617f9d4822c08e3cf74acb4dae191cfac8
Derek <derekbohnet@gmail.com>
2018-11-06 15:04:13 -0800

    determinant sudo code class added

---
+++ src/Math/Determinant.java
@@ -0,0 +1,26 @@
+package Math;
+
+public class Determinant {
+	private void cofactor(MatrixND) {
+		if(basecase)
+			return
+		for each subMatrix
+			determinant(subMatrix);
+				=+-+-+-
+	}
+	
+	public float determinant(Matrix3D) {
+		return 0.0f;
+	}
+	
+	public float determinant(Matrix2D mat2d) {
+		cofactor(mat2d);
+		return 0.0f;
+	}
+	
+	public float determinant(Matrix1D) {
+		do trivial determinant for 2x2
+		return 0.0f;
+	}
+	
+}
